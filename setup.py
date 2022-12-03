175a8dfed6c573ec44f581f7e656b2a32b49fd2b
vit017 <kaluzhskikh.v@gmail.com>
2022-12-04 00:29:39 +0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='devskill1',
+      version='1.0',
+      description='Python Test Pack',
+      author='vit017',
+      author_email='kaluzhskikh.v@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=[],
+     )
